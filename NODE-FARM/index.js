b6ffcb910959b6d4b31bad96cb6fbd9b736d110c
Denis Hristov <48285397+DenisHristov12@users.noreply.github.com>
2025-03-21 20:14:13 +0200

    read fyle async

--- NODE-FARM/index.js
+++ NODE-FARM/index.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
 
+// Blocking, synchronous way
+
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8');
 
 console.log(textIn);
@@ -7,3 +9,9 @@ console.log(textIn);
 const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
 fs.writeFileSync('./txt/output.txt', textOut);
 console.log('File written');
+
+// Non-blocking, asynchronous way
+
+fs.readFile('./txt/start.txt', (err, data) => {
+  console.log(data);
+});
