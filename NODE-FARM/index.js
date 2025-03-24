7e763ca92d5ec39e92843160c6845f6af2379d0e
Denis Hristov <48285397+DenisHristov12@users.noreply.github.com>
2025-03-24 19:04:01 +0200

    write and read files asynchronously

--- NODE-FARM/index.js
+++ NODE-FARM/index.js
@@ -12,6 +12,19 @@ console.log('File written');
 
 // Non-blocking, asynchronous way
 
-fs.readFile('./txt/start.txt', (err, data) => {
-  console.log(data);
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+  if (err) {
+    return console.log('ERROR');
+  }
+
+  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+    console.log(data2);
+    fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+        console.log('File has been written!');
+      });
+    });
+  });
 });
