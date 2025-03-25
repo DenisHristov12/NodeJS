a815934651b7bf696adae057b7911afb4f5ce091
Denis Hristov <48285397+DenisHristov12@users.noreply.github.com>
2025-03-25 15:38:16 +0200

    add some packages

--- NODE-FARM/index.js
+++ NODE-FARM/index.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const http = require('http');
 const url = require('url');
 
+const slugify = require('slugify');
+
 const replaceTemplate = require('./modules/replaceTemplate');
 
 ///////////////////////////////////////
@@ -51,6 +53,9 @@ const tempCard = fs.readFileSync(`${__dirname}/templates/card.html`, 'utf-8');
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lowert: true }));
+console.log(slugs);
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
